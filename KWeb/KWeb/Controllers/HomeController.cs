edaa79e90461ee9cdff770dc8701c9811034e175
emora70111 <163953335+emora70111@users.noreply.github.com>
2024-09-30 13:03:25 -0600

    version inicial del proyecto

---
+++ KWeb/KWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Appoinment()
+        {
+            return View();  // ASP.NET MVC buscará automáticamente la vista "Appoinment.cshtml"
+        }
+    }
+}
---
+++ KWeb/KWeb/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult RegistroUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+    }
+}
