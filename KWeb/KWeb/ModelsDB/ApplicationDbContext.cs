f6baecbcad98bff3ebeb48e57e8f61eea05f422b
emora70111 <emora70111@ufide.ac.cr>
2024-11-19 17:35:41 -0500

    feat: Inicio de Modulo 3 y funcionalidades

--- KWeb/KWeb/ModelsDB/ApplicationDbContext.cs
+++ KWeb/KWeb/ModelsDB/ApplicationDbContext.cs
@@ -12,9 +12,28 @@ namespace KWeb.ModelsDB
         {
         }
 
-        public DbSet<Usuario> Usuarios { get; set; } }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Doctor> Doctores { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Cita> Citas { get; set; }
 
-       
-    
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Cita>()
+                .HasRequired(c => c.Doctor)
+                .WithMany(d => d.Citas)
+                .HasForeignKey(c => c.DoctorId);
+
+            modelBuilder.Entity<Cita>()
+                .HasRequired(c => c.Paciente)
+                .WithMany(p => p.Citas)
+                .HasForeignKey(c => c.PacienteId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
           
 }
---
+++ KWeb/KWeb/ModelsDB/Cita.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KWeb.ModelsDB
+{
+    public class Cita
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int PacienteId { get; set; }
+
+        [ForeignKey("PacienteId")]
+        public virtual Paciente Paciente { get; set; }
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [ForeignKey("DoctorId")]
+        public virtual Doctor Doctor { get; set; }
+
+        [Required]
+        public DateTime FechaHora { get; set; }
+    }
+}
